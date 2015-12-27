53000599179cf8ce873d555d2926aeb847e0f6e0
Masahiko Tsujita <tsujitamasahiko.dev@icloud.com>
2015-12-27 16:45:37 +0900

    Made presentation of notification generic

--- Pod/Classes/Notification.swift
+++ Pod/Classes/Notification.swift
@@ -47,7 +47,71 @@ public enum HeightType {
 
 public typealias AccessoryViewAlignment = CRToastAccessoryViewAlignment
 
-public class Notification {
+public protocol NotificationType {
+
+    var text: String { get }
+
+    var textAlignment: NSTextAlignment { get }
+
+    var textFont: UIFont { get }
+
+    var textColor: UIColor { get }
+
+    var textMaxNumberOfLines: Int { get }
+
+    var textShadowColor: UIColor { get }
+
+    var textShadowOffset: CGSize { get }
+
+    var subtext: String? { get }
+
+    var subtextAlignment: NSTextAlignment { get }
+
+    var subtextFont: UIFont { get }
+
+    var subtextColor: UIColor { get }
+
+    var subtextMaxNumberOfLines: Int { get }
+
+    var subtextShadowColor: UIColor { get }
+
+    var subtextShadowOffset: CGSize { get }
+
+    var heightType: HeightType { get }
+
+    var underStatusBar: Bool { get }
+
+    var statusBarStyle: UIStatusBarStyle { get }
+
+    var preferredPadding: CGFloat { get }
+
+    var image: UIImage? { get }
+
+    var imageTintColor: UIColor? { get }
+
+    var imageAlignment: CRToastAccessoryViewAlignment { get }
+
+    var imageContentMode: UIViewContentMode { get }
+
+    var backgroundColor: UIColor { get }
+
+    var backgroundView: UIView? { get }
+
+    var activityIndicatorVisible: Bool { get }
+
+    var activityIndicatorViewStyle: UIActivityIndicatorViewStyle { get }
+
+    var activityIndicatorAlignment: AccessoryViewAlignment { get }
+
+    var keepsNavigationBarBorder: Bool { get }
+
+    var rotatesAutomatically: Bool { get }
+
+    var capturesDefaultWindow: Bool { get }
+
+}
+
+public class Notification: NotificationType {
     
     public init(text: String = "", subtext: String? = nil) {
         self.text       = text
@@ -116,108 +180,112 @@ public class Notification {
     
 }
 
-public func notify(notification: Notification, animation: Animation = .Linear, lifetime: TimeInterval = .Finite(2.0), handler: () -> Void) -> Presentation {
+public extension NotificationType {
     
-    // Initializing Presentation Objects and Configurings
-    
-    let identifier = NSUUID().UUIDString
-    let presentation = Presentation()
-    let dismisser = NotificationDismisser(identifier: identifier)
-    
-    var options = [String : AnyObject]()
-    options[kCRToastIdentifierKey]                      = identifier
-    
-    // Configuring Texts
-    
-    options[kCRToastTextKey]                            = notification.text
-    options[kCRToastTextAlignmentKey]                   = notification.textAlignment.rawValue
-    options[kCRToastFontKey]                            = notification.textFont
-    options[kCRToastTextColorKey]                       = notification.textColor
-    options[kCRToastTextMaxNumberOfLinesKey]            = notification.textMaxNumberOfLines
-    options[kCRToastTextShadowColorKey]                 = notification.textShadowColor
-    options[kCRToastTextShadowOffsetKey]                = NSValue(CGSize: notification.textShadowOffset)
-    
-    if notification.subtext != nil {
-        options[kCRToastSubtitleTextKey]                = notification.subtext
+    public func notify(animation: Animation = .Linear, lifetime: TimeInterval = .Finite(2.0), handler: () -> Void) -> Presentation<Self> {
+        
+        // Initializing Presentation Objects and Configurings
+        
+        let identifier = NSUUID().UUIDString
+        let presentation = Presentation(notification: self)
+        let dismisser = NotificationDismisser(identifier: identifier)
+        
+        var options = [String : AnyObject]()
+        options[kCRToastIdentifierKey]                      = identifier
+        
+        // Configuring Texts
+        
+        options[kCRToastTextKey]                            = self.text
+        options[kCRToastTextAlignmentKey]                   = self.textAlignment.rawValue
+        options[kCRToastFontKey]                            = self.textFont
+        options[kCRToastTextColorKey]                       = self.textColor
+        options[kCRToastTextMaxNumberOfLinesKey]            = self.textMaxNumberOfLines
+        options[kCRToastTextShadowColorKey]                 = self.textShadowColor
+        options[kCRToastTextShadowOffsetKey]                = NSValue(CGSize: self.textShadowOffset)
+        
+        if self.subtext != nil {
+            options[kCRToastSubtitleTextKey]                = self.subtext
+        }
+        options[kCRToastSubtitleTextAlignmentKey]           = self.subtextAlignment.rawValue
+        options[kCRToastSubtitleFontKey]                    = self.subtextFont
+        options[kCRToastSubtitleTextColorKey]               = self.subtextColor
+        options[kCRToastSubtitleTextMaxNumberOfLinesKey]    = self.subtextMaxNumberOfLines
+        options[kCRToastSubtitleTextShadowColorKey]         = self.subtextShadowColor
+        options[kCRToastSubtitleTextShadowOffsetKey]        = NSValue(CGSize: self.subtextShadowOffset)
+        
+        // Configuring Appearances
+        
+        options[kCRToastNotificationTypeKey]                = self.heightType.crToastType.rawValue
+        switch self.heightType {
+        case .Custom(let preferredHeight):
+            options[kCRToastNotificationPreferredHeightKey] = preferredHeight
+        default:
+            break
+        }
+        
+        options[kCRToastBackgroundColorKey]                 = self.backgroundColor
+        options[kCRToastBackgroundViewKey]                  = self.backgroundView
+        
+        options[kCRToastNotificationPreferredPaddingKey]    = self.preferredPadding
+        
+        options[kCRToastUnderStatusBarKey]                  = self.underStatusBar
+        options[kCRToastStatusBarStyleKey]                  = self.statusBarStyle.rawValue
+        
+        options[kCRToastImageKey]                           = self.image
+        options[kCRToastImageTintKey]                       = self.imageTintColor
+        options[kCRToastImageAlignmentKey]                  = self.imageAlignment.rawValue
+        options[kCRToastImageContentModeKey]                = self.imageContentMode.rawValue
+        
+        options[kCRToastShowActivityIndicatorKey]           = self.activityIndicatorVisible
+        options[kCRToastActivityIndicatorAlignmentKey]      = self.activityIndicatorAlignment.rawValue
+        options[kCRToastActivityIndicatorViewStyleKey]      = self.activityIndicatorViewStyle.rawValue
+        
+        options[kCRToastKeepNavigationBarBorderKey]         = self.keepsNavigationBarBorder
+        
+        // Configuring Other Properties
+        
+        options[kCRToastAutorotateKey]                      = self.rotatesAutomatically
+        options[kCRToastCaptureDefaultWindowKey]            = self.capturesDefaultWindow
+        
+        // Configuring Animations
+        
+        options[kCRToastAnimationInTypeKey]                 = animation.inType.rawValue
+        options[kCRToastAnimationInDirectionKey]            = animation.inDirection.rawValue
+        options[kCRToastAnimationInTimeIntervalKey]         = animation.inDuration
+        
+        options[kCRToastAnimationOutTypeKey]                = animation.outType.rawValue
+        options[kCRToastAnimationOutDirectionKey]           = animation.outDirection.rawValue
+        options[kCRToastAnimationOutTimeIntervalKey]        = animation.outDuration
+        
+        options[kCRToastAnimationSpringDampingKey]          = animation.springDamping
+        options[kCRToastAnimationSpringInitialVelocityKey]  = animation.springInitialVelocity
+        options[kCRToastAnimationGravityMagnitudeKey]       = animation.gravityMagnitude
+        
+        options[kCRToastNotificationPresentationTypeKey]    = animation.presentationType.rawValue
+        
+        // Configuring User Interactions
+        
+        switch lifetime {
+        case .Finite(let timeInterval):
+            options[kCRToastForceUserInteractionKey]        = false
+            options[kCRToastTimeIntervalKey]                = timeInterval
+        case .Infinite:
+            options[kCRToastForceUserInteractionKey]        = true
+        }
+        
+        options[kCRToastInteractionRespondersKey]           = [CRToastInteractionResponder(interactionType: .All, automaticallyDismiss: false, block: { type in
+            presentation.interactionEvent.invoke((type, self, dismisser))
+        })]
+        
+        // Presenting Notification
+        
+        dispatch_async(dispatch_get_main_queue()) {
+            CRToastManager.showNotificationWithOptions(options, apperanceBlock: handler, completionBlock: {
+                presentation.dismissalEvent.invoke(self)
+            })
+        }
+        
+        return presentation
     }
-    options[kCRToastSubtitleTextAlignmentKey]           = notification.subtextAlignment.rawValue
-    options[kCRToastSubtitleFontKey]                    = notification.subtextFont
-    options[kCRToastSubtitleTextColorKey]               = notification.subtextColor
-    options[kCRToastSubtitleTextMaxNumberOfLinesKey]    = notification.subtextMaxNumberOfLines
-    options[kCRToastSubtitleTextShadowColorKey]         = notification.subtextShadowColor
-    options[kCRToastSubtitleTextShadowOffsetKey]        = NSValue(CGSize: notification.subtextShadowOffset)
     
-    // Configuring Appearances
-    
-    options[kCRToastNotificationTypeKey]                = notification.heightType.crToastType.rawValue
-    switch notification.heightType {
-    case .Custom(let preferredHeight):
-        options[kCRToastNotificationPreferredHeightKey] = preferredHeight
-    default:
-        break
-    }
-    
-    options[kCRToastBackgroundColorKey]                 = notification.backgroundColor
-    options[kCRToastBackgroundViewKey]                  = notification.backgroundView
-    
-    options[kCRToastNotificationPreferredPaddingKey]    = notification.preferredPadding
-    
-    options[kCRToastUnderStatusBarKey]                  = notification.underStatusBar
-    options[kCRToastStatusBarStyleKey]                  = notification.statusBarStyle.rawValue
-    
-    options[kCRToastImageKey]                           = notification.image
-    options[kCRToastImageTintKey]                       = notification.imageTintColor
-    options[kCRToastImageAlignmentKey]                  = notification.imageAlignment.rawValue
-    options[kCRToastImageContentModeKey]                = notification.imageContentMode.rawValue
-    
-    options[kCRToastShowActivityIndicatorKey]           = notification.activityIndicatorVisible
-    options[kCRToastActivityIndicatorAlignmentKey]      = notification.activityIndicatorAlignment.rawValue
-    options[kCRToastActivityIndicatorViewStyleKey]      = notification.activityIndicatorViewStyle.rawValue
-    
-    options[kCRToastKeepNavigationBarBorderKey]         = notification.keepsNavigationBarBorder
-    
-    // Configuring Other Properties
-    
-    options[kCRToastAutorotateKey]                      = notification.rotatesAutomatically
-    options[kCRToastCaptureDefaultWindowKey]            = notification.capturesDefaultWindow
-    
-    // Configuring Animations
-    
-    options[kCRToastAnimationInTypeKey]                 = animation.inType.rawValue
-    options[kCRToastAnimationInDirectionKey]            = animation.inDirection.rawValue
-    options[kCRToastAnimationInTimeIntervalKey]         = animation.inDuration
-    
-    options[kCRToastAnimationOutTypeKey]                = animation.outType.rawValue
-    options[kCRToastAnimationOutDirectionKey]           = animation.outDirection.rawValue
-    options[kCRToastAnimationOutTimeIntervalKey]        = animation.outDuration
-    
-    options[kCRToastAnimationSpringDampingKey]          = animation.springDamping
-    options[kCRToastAnimationSpringInitialVelocityKey]  = animation.springInitialVelocity
-    options[kCRToastAnimationGravityMagnitudeKey]       = animation.gravityMagnitude
-    
-    options[kCRToastNotificationPresentationTypeKey]    = animation.presentationType.rawValue
-    
-    // Configuring User Interactions
-    
-    switch lifetime {
-    case .Finite(let timeInterval):
-        options[kCRToastForceUserInteractionKey]        = false
-        options[kCRToastTimeIntervalKey]                = timeInterval
-    case .Infinite:
-        options[kCRToastForceUserInteractionKey]        = true
-    }
-    
-    options[kCRToastInteractionRespondersKey]           = [CRToastInteractionResponder(interactionType: .All, automaticallyDismiss: false, block: { type in
-        presentation.interactionEvent.invoke((type, notification, dismisser))
-    })]
-    
-    // Presenting Notification
-    
-    dispatch_async(dispatch_get_main_queue()) {
-        CRToastManager.showNotificationWithOptions(options, apperanceBlock: handler, completionBlock: {
-            presentation.dismissalEvent.invoke(notification)
-        })
-    }
-    
-    return presentation
 }
