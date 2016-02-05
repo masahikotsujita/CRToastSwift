cad2fe7902953f7a68b73c07ca83940ba3a073da
Masahiko Tsujita <tsujitamasahiko.dev@icloud.com>
2016-02-05 22:46:23 +0900

    Moved NotificationTraits into separate file

--- Pod/Classes/Notification.swift
+++ Pod/Classes/Notification.swift
@@ -24,7 +24,6 @@
 //
 
 import UIKit
-import CRToast
 
 public protocol NotificationType {
     
@@ -56,90 +55,5 @@ extension String: NotificationType {
     public var subtext: String? {
         return nil
     }
-    
-}
-
-public struct NotificationTraits {
-    
-    public typealias AccessoryViewAlignment = CRToastAccessoryViewAlignment
-    
-    public init() {
-        
-    }
-    
-    public var textAlignment: NSTextAlignment = .Left
-    
-    public var textFont = UIFont.boldSystemFontOfSize(18.0)
-    
-    public var textColor: Adaptable<UIColor> = .Adapting
-    
-    public var textMaxNumberOfLines: Int = 0
-    
-    public var textShadowColor: UIColor = UIColor.clearColor()
-    
-    public var textShadowOffset: CGSize = CGSize(width: 0.0, height: 0.0)
-    
-    public var subtextAlignment: NSTextAlignment = .Left
-    
-    public var subtextFont = UIFont.systemFontOfSize(14.0)
-    
-    public var subtextColor: Adaptable<UIColor> = .Adapting
-    
-    public var subtextMaxNumberOfLines: Int = 0
-    
-    public var subtextShadowColor: UIColor = UIColor.clearColor()
-    
-    public var subtextShadowOffset: CGSize = CGSize(width: 0.0, height: 0.0)
-    
-    public enum Size {
-        
-        case Compact
-        case Regular
-        case Custom(preferredHeight: CGFloat)
-        
-        var crToastType: CRToastType {
-            switch self {
-            case .Compact:
-                return .StatusBar
-            case .Regular:
-                return .NavigationBar
-            case .Custom(_):
-                return .Custom
-            }
-        }
-        
-    }
-    
-    public var size: Size = .Regular
-    
-    public var showsStatusBar = false
-    
-    public var statusBarStyle: Adaptable<UIStatusBarStyle> = .Adapting
-    
-    public var preferredPadding: CGFloat = 0
-    
-    public var image: UIImage?
-    
-    public var imageTintColor: Adaptable<UIColor>?
-    
-    public var imageAlignment: CRToastAccessoryViewAlignment = .Left
-    
-    public var imageContentMode: UIViewContentMode = .Center
-    
-    public var backgroundColor = UIColor.darkGrayColor()
-    
-    public var backgroundView: (() -> UIView?)?
-    
-    public var showsActivityIndicatorView = false
-    
-    public var activityIndicatorViewStyle: Adaptable<UIActivityIndicatorViewStyle> = .Adapting
-    
-    public var activityIndicatorAlignment: AccessoryViewAlignment = .Left
-    
-    public var keepsNavigationBarBorder = true
-    
-    public var rotatesAutomatically = true
-    
-    public var capturesDefaultWindow = true
     
 }
