14599e3a81fbefbd0c8614328f0958653fb98f83
Masahiko Tsujita <tsujitamasahiko.dev@icloud.com>
2016-03-21 02:36:38 +0900

    Remove Example and cleanup file tree

---
+++ CRToastSwift/CRToastSwift.h
@@ -0,0 +1,19 @@
+//
+//  CRToastSwift.h
+//  CRToastSwift
+//
+//  Created by Masahiko Tsujita on 2016/03/21.
+//  Copyright © 2016年 Masahiko Tsujita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CRToastSwift.
+FOUNDATION_EXPORT double CRToastSwiftVersionNumber;
+
+//! Project version string for CRToastSwift.
+FOUNDATION_EXPORT const unsigned char CRToastSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CRToastSwift/PublicHeader.h>
+
+
--- Example/Pods/Target Support Files/CRToast/CRToast-umbrella.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "CRToast.h"
-#import "CRToastConfig.h"
-#import "CRToastManager.h"
-
-FOUNDATION_EXPORT double CRToastVersionNumber;
-FOUNDATION_EXPORT const unsigned char CRToastVersionString[];
-
